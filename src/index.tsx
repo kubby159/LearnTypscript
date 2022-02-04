d9ba40430ad1d10f614ed9a878f14c5ef535dd66
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-04 18:06:59 +0900

    [TS] Typing the Props

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
-import App from "./App";
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={false ? darkTheme : lightTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
