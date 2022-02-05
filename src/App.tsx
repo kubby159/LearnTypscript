130e12bb46afcaefab0d6e70f331c17f36a2c7c2
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-05 22:17:59 +0900

    [TS] Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
-import Circle from "./Circle";
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    //event : any Type
+    //어떤 종류의 Element가 onChange 이벤트를 발생시킬지를 특정할 수 있음.
+    //위 코드는 HTMLInputElement에 의해서 onChange가 실행됨.
+    //React JS의 TS에서는 target 대신 currentTarget을 이용
+
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
-    <>
-      <Circle text="stupid" borderColor="blue" bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log ing</button>
+      </form>
+    </div>
   );
 }
 
