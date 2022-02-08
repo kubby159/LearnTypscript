955af7c7b4290d8bc01652cbcd6e8a0e90f748e5
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-08 13:34:09 +0900

    [React] styles setting

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -6,6 +6,8 @@ interface RouteParams {
 function Coin() {
   const { coinId } = useParams<RouteParams>();
   //http://localhost:3000/btc 의 btc 부분을 가지고옴.
+  //TS : useParams를 이용 시 타입을 안붙여주면 TS는 {} 데이터로 인식하게 되어 오류를 발생시킨다.
+  //그러므로 위와 같이 타입을 지정해줘야함.
   return <h1>Coin : {coinId}</h1>;
 }
 
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,11 +1,9 @@
 import "styled-components";
 
-//and extend them!
-
-declare module "style-components" {
+declare module "styled-components" {
   export interface DefaultTheme {
     textColor: string;
     bgColor: string;
-    btnColor: string;
+    accentColor: string;
   }
 }
