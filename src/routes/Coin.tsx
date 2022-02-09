8480f0e215064f0dc14d9897a39d12dbf862d6fd
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-09 19:52:53 +0900

    [React] coinData

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
 interface RouteParams {
@@ -32,18 +32,33 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
+const API_URL = "https://api.coinpaprika.com/v1/coins";
+const API_URL_PRICE = "https://api.coinpaprika.com/v1/tickers";
 function Coin() {
-  const { coinId } = useParams<RouteParams>();
-
   const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
+  // useState 타입을 지정해줘야함.
+  // 매번 타입을 지정해줄 필요 없이 자동생성할 수 있음<div className=""></div>
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (await fetch(`${API_URL}/${coinId}`)).json();
+      const priceData = await (
+        await fetch(`${API_URL_PRICE}/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
       <Header>
         <Title>{state.name.toUpperCase() || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>{`Loading...`}</Loader> : null}{" "}
+      {loading ? <Loader>{`Loading...`}</Loader> : <span></span>}
     </Container>
   );
 }
