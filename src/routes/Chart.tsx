d5d8859300fe469abd1d7f6bf32701042265c394
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-16 02:29:43 +0900

    [React] making Chart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from "react-query";
+import { useParams } from "react-router";
 import { fetchCoinHistory } from "./api";
 import ApexChart from "react-apexcharts";
+import { useState } from "react";
 interface IHistorical {
   time_open: string;
   time_close: string;
@@ -17,70 +19,64 @@ interface ChartProps {
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery<IHistorical[]>(
-    ["ohlcv", coinId],
-    () => fetchCoinHistory(coinId),
-    {
-      refetchInterval: 10000,
-    }
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
   );
+  // {
+  //   name: "candle",
+  //   data: [
+  //     {
+  //       x: null,
+  //       y: null,
+  //     },
+  //   ],
+  // },
   return (
     <div>
       {isLoading ? (
-        "Loading chart...."
+        "Loading.."
       ) : (
         <ApexChart
-          type="line"
-          series={[
-            {
-              name: "Price",
-              data: data?.map((price) => price.close),
-            },
-          ]}
+          type="candlestick"
+          series={data?.map((e) => {
+            return {
+              data: [{ x: e.time_open, y: [e.open, e.high, e.low, e.close] }],
+            };
+          })}
           options={{
             chart: {
-              height: 300,
+              height: 350,
               width: 500,
-              toolbar: {
-                show: false,
-              },
               background: "transparent",
             },
-            grid: {
-              show: false,
+            title: {
+              text: "CandleStick Chart",
+              align: "left",
             },
             theme: {
               mode: "dark",
             },
-            stroke: {
-              curve: "smooth",
-              width: 4,
-            },
             xaxis: {
-              labels: {
-                show: false,
-              },
-              axisBorder: {
-                show: false,
-              },
-              axisTicks: {
-                show: false,
-              },
               type: "datetime",
-              categories: data?.map((e) => e.time_close),
             },
             yaxis: {
-              show: false,
+              tooltip: {
+                enabled: true,
+              },
+            },
+            plotOptions: {
+              candlestick: {
+                colors: {
+                  upward: "#00B746",
+                  downward: "#EF403C",
+                },
+                wick: {
+                  useFillColor: true,
+                },
+              },
             },
             fill: {
-              type: "gradient",
-              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
-            },
-            colors: ["#0fbcf9"],
-            tooltip: {
-              y: {
-                formatter: (value) => `$ ${value.toFixed(2)}`,
-              },
+              colors: ["#1A73E8", "#B32824"],
             },
           }}
         />
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,28 @@
-function Price() {
-  return <h1>Price</h1>;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+interface ChartProps {
+  coinId: string;
+}
+
+function Price({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    { refetchInterval: 10000 }
+  );
+
+  return <div></div>;
 }
 
 export default Price;
